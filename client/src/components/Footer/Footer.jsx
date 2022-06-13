c3d7b36c1c4b9f9cc1eb1f9774670397cce06d42
Aryse Tansy <medictansy@gmail.com>
2022-06-13 11:09:22 -0400

    Final adjustment design

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import classes from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <small className={classes.footer}>
+      <small className={classes.small}>
         Medic1111 {new Date().getFullYear()} Copyrights
       </small>
     </footer>
