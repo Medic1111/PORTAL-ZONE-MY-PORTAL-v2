ebfcc39e6f94fc1091a11869fa553d9c2dcac0a5
Aryse Tansy <medictansy@gmail.com>
2022-06-16 16:05:53 -0400

    Testing some main user page css on mobile

--- client/src/components/StudentMain/StudentMain.jsx
+++ client/src/components/StudentMain/StudentMain.jsx
@@ -11,7 +11,7 @@ const StudentMain = () => {
         <div className={classes.divComp}>
           <UserMainAside />
 
-          {/* <section>SECTION</section> */}
+          <section className={classes.test}>SECTION SECTION SECTION</section>
         </div>
       </div>
     </main>
