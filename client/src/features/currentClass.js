f52a3433793b96843ed5052ecfe256e801c04712
Aryse Tansy <medictansy@gmail.com>
2022-06-20 16:28:55 -0400

    Finish clean up

--- client/src/features/currentClass.js
+++ client/src/features/currentClass.js
@@ -17,17 +17,6 @@ export const currentClass = createSlice({
     },
     updateMessages: (state, action) => {
       state.class.messages = [...state.class.messages, action.payload];
-      // state.class.messages = [action.payload];
-      // let toBePushed = action.payload;
-      // ALL MESSAGES IS THE LAST SNAP
-      // OF MESSAGES STATE.
-      // MUST STORE IN VAR
-      // let allMsgs = state.class.messages;
-      // state.class.messages = [...allMsgs, toBePushed];
-
-      //
-      //
-      // WILL ATTEMPT WITH STATE AND DUMMY ARR
     },
     clearClass: (state) => {
       state.class = {
